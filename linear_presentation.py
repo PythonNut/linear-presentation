c1820ae64da84326741c72c06525ef5c7acf8262
Forest Kobayashi <fkobayashi@hmc.edu>
2019-07-23 23:19:38 -0700

    blacken buffers

--- linear_presentation.py
+++ linear_presentation.py
@@ -1,14 +1,48 @@
-import numpy as np
+# ============================================================================ #
+#                                                                              #
+#                              Linear Presentation                             #
+#                                                                              #
+# ============================================================================ #
+#
+#
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+#                     ┌───────────┐                                            #
+#                     │           │                                            #
+#        ┌───┐        │   ┌───┐   │                                            #
+#        │   │        │   │   │   │                                            #
+#    ┌───────┘    ┌───│───────│───┘                                            #
+#    │   │        │   │   │   │                                                #
+#    └───┘        │   └───┘   │                                                #
+#                 │           │                                                #
+#                 └───────────┘                                                #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+#
+# If you want to build your own, try these
+# ┌ └ ┐ ┘ ├ ┤ ┬ ┴ ┼ │ ─
+
 from gauss_codes import gknot
 import os
-import copy
+
+
+################################################################################
+#                                                                              #
+#                               Helper Functions                               #
+#                                                                              #
+################################################################################
+
 
 def sign(i):
+    """
+    FIXME why is there a return 0?
+    """
     if i > 0:
         return 1
     elif i < 0:
         return -1
     return 0
+
 
 def get_cnum(c):
     """
@@ -34,12 +68,26 @@ def get_cnum(c):
 
     return int(abs(c))
 
+
 def decompose_c(c):
+    """
+    In our initial path construction, routes can take many shapes:
+
+         ┌──┐           ┌───────┐
+         │  │           │       │
+    x    │  y           x       y
+    │    │
+    └────┘
+
+    Note, we can decompose all of these paths into c shapes
+
+    """
     over_under = 1 if c > 0 else -1
     c = abs(c)
     pos_neg = 1 if c % 1 == 0 else -1
     cnum = int(abs(c))
     return cnum, over_under, pos_neg
+
 
 def recompose_c(cnum, over_under, pos_neg):
     if pos_neg == -1:
@@ -47,6 +95,7 @@ def recompose_c(cnum, over_under, pos_neg):
     if over_under == -1:
         cnum *= -1
     return cnum
+
 
 def get_y_in(cross_seen, c):
     """
@@ -118,9 +167,9 @@ def add_c_shape(path, x1, x2, y_sign):
 def get_c_shapes(path):
     intervals = []
     for p1, p2 in zip(path, path[1:]):
-       (p1x, p1y), (p2x, p2y) = p1, p2
-       if p1y == p2y:
-           intervals.append((min(p1x, p2x), max(p1x, p2x), sign(p1y)))
+        (p1x, p1y), (p2x, p2y) = p1, p2
+        if p1y == p2y:
+            intervals.append((min(p1x, p2x), max(p1x, p2x), sign(p1y)))
 
     return intervals
 
@@ -128,7 +177,7 @@ def get_c_shapes(path):
 def get_envelope(c_shapes, x, y):
     y = sign(y)
     assert y != 0
-    min_size, min_c = float('inf'), None
+    min_size, min_c = float("inf"), None
     for (cx1, cx2, cy) in c_shapes:
         if cy == y and cx1 < x and x < cx2:
             size = cx2 - cx1
@@ -153,6 +202,7 @@ def get_envelope(c_shapes, x, y):
 #         x_shift = c2 - r_gap_c + 1
 #         return r_gap_c * (n + 3) - x_shift
 
+
 def get_gap_x(c1, c2, n):
     assert c1 != c2
     if c1 < c2:
@@ -161,6 +211,7 @@ def get_gap_x(c1, c2, n):
     else:
         # Suspect this never actually occurs
         return c2 * (n + 3) + 1 + n - c1
+
 
 def find_gap(cross_x, envelope):
     print(envelope)
@@ -174,8 +225,9 @@ def find_gap(cross_x, envelope):
 
     return len(cross_x)
 
+
 def check_gap(c_shapes, cross_x, gapi):
-    gx1 = cross_x[gapi-1]
+    gx1 = cross_x[gapi - 1]
 
     # TODO: this is sketch
     if gapi < len(cross_x):
@@ -190,6 +242,7 @@ def check_gap(c_shapes, cross_x, gapi):
 
     return True
 
+
 def escape(c_shapes, cross_x, x, y):
     n = len(cross_x)
     e = get_envelope(c_shapes, x, y)
@@ -200,13 +253,14 @@ def escape(c_shapes, cross_x, x, y):
 
     if check_gap(c_shapes, cross_x, gapi):
         # calculate c1 from x and c2 from gap
-        c1 = x//(n + 3)
+        c1 = x // (n + 3)
         c2 = gapi
         x2 = get_gap_x(c1, c2, n)
         return [(*e, x2)] + escape(c_shapes, cross_x, x2, -y)
 
     else:
         return [e]
+
 
 def normalize_gauss_order(gcode):
     crossings_map = {}
@@ -218,13 +272,10 @@ def normalize_gauss_order(gcode):
             top_cross += 1
             crossings_map[cnum] = top_cross
 
-        new_gcode.append(recompose_c(
-            crossings_map[cnum],
-            over_under,
-            pos_neg
-        ))
+        new_gcode.append(recompose_c(crossings_map[cnum], over_under, pos_neg))
 
     return new_gcode
+
 
 def get_path(path, cross_x, c1i, c2i, x1, x2, y1, y2):
     n = len(cross_x)
@@ -259,18 +310,36 @@ def get_path(path, cross_x, c1i, c2i, x1, x2, y1, y2):
                 if len(esc1) >= len(esc2):
                     # we break symmetry arbitrarily
                     x_psuedo = esc1[0][2]
-                    print('recurse!')
+                    print("recurse!")
                     add_c_shape(path, x1, x_psuedo, y1)
-                    get_path(path, cross_x, find_gap(cross_x, (0, x_psuedo)), c2i, x_psuedo, x2, -y1, y2)
+                    get_path(
+                        path,
+                        cross_x,
+                        find_gap(cross_x, (0, x_psuedo)),
+                        c2i,
+                        x_psuedo,
+                        x2,
+                        -y1,
+                        y2,
+                    )
 
                 else:
                     x_psuedo = esc2[0][2]
                     # Ok, so this time we go in reverse
-                    print('recurse!')
-                    get_path(path, cross_x, c1i, find_gap(cross_x, (0, x_psuedo)), x1, x_psuedo, y1, -y2)
+                    print("recurse!")
+                    get_path(
+                        path,
+                        cross_x,
+                        c1i,
+                        find_gap(cross_x, (0, x_psuedo)),
+                        x1,
+                        x_psuedo,
+                        y1,
+                        -y2,
+                    )
                     add_c_shape(path, x_psuedo, x2, y2)
 
-                print('oh no1')
+                print("oh no1")
 
     else:
         if y1 == 0:
@@ -284,7 +353,7 @@ def get_path(path, cross_x, c1i, c2i, x1, x2, y1, y2):
             e2 = get_envelope(c_shapes, x2, y2)
             if e1 == e2:
                 # base case
-                add_c_shape(path, x1+1, x2, y2)
+                add_c_shape(path, x1 + 1, x2, y2)
 
             else:
                 # We're backtracking, so the source must be "open to
@@ -297,10 +366,19 @@ def get_path(path, cross_x, c1i, c2i, x1, x2, y1, y2):
                 print(esc2)
 
                 x_psuedo = esc2[0][2]
-                print('recurse!')
-                get_path(path, cross_x, c1i, find_gap(cross_x, (0, x_psuedo)), x1, x_psuedo, y1, -y2)
+                print("recurse!")
+                get_path(
+                    path,
+                    cross_x,
+                    c1i,
+                    find_gap(cross_x, (0, x_psuedo)),
+                    x1,
+                    x_psuedo,
+                    y1,
+                    -y2,
+                )
                 add_c_shape(path, x_psuedo, x2, y2)
-                print('oh no2')
+                print("oh no2")
 
         elif y2 == 0:
             # CASE 3
@@ -321,8 +399,8 @@ def get_path(path, cross_x, c1i, c2i, x1, x2, y1, y2):
                 # destination is "open to the air", although we don't
                 # know which direction is open. Therefore, all of the
                 # escaping is done on the source end.
-                esc2u = escape(c_shapes, cross_x, x2-1, +1)
-                esc2d = escape(c_shapes, cross_x, x2-1, -1)
+                esc2u = escape(c_shapes, cross_x, x2 - 1, +1)
+                esc2d = escape(c_shapes, cross_x, x2 - 1, -1)
                 assert esc2u == [None] or esc2d == [None]
 
                 esc1 = escape(c_shapes, cross_x, x1, y1)
@@ -331,9 +409,18 @@ def get_path(path, cross_x, c1i, c2i, x1, x2, y1, y2):
                 x_psuedo = esc1[0][2]
                 add_c_shape(path, x1, x_psuedo, y1)
                 print("recurse!")
-                get_path(path, cross_x, find_gap(cross_x, (0, x_psuedo)), c2i, x_psuedo, x2-1, -y1, y2)
+                get_path(
+                    path,
+                    cross_x,
+                    find_gap(cross_x, (0, x_psuedo)),
+                    c2i,
+                    x_psuedo,
+                    x2 - 1,
+                    -y1,
+                    y2,
+                )
 
-                print('oh no3')
+                print("oh no3")
 
             path.append((x2, y2))
 
@@ -371,17 +458,34 @@ def get_path(path, cross_x, c1i, c2i, x1, x2, y1, y2):
                 if len(esc1) >= len(esc2):
                     # we break symmetry arbitrarily
                     x_psuedo = esc1[0][2]
-                    print('recurse!')
+                    print("recurse!")
                     add_c_shape(path, x1, x_psuedo, y1)
-                    get_path(path, cross_x, find_gap(cross_x, (0, x_psuedo)), c2i, x_psuedo, x2, -y1, y2)
+                    get_path(
+                        path,
+                        cross_x,
+                        find_gap(cross_x, (0, x_psuedo)),
+                        c2i,
+                        x_psuedo,
+                        x2,
+                        -y1,
+                        y2,
+                    )
 
                 else:
                     x_psuedo = esc2[0][2]
                     # Ok, so this time we go in reverse
-                    print('recurse!')
-                    get_path(path, cross_x, c1i, find_gap(cross_x, (0, x_psuedo)), x1, x_psuedo, y1, -y2)
+                    print("recurse!")
+                    get_path(
+                        path,
+                        cross_x,
+                        c1i,
+                        find_gap(cross_x, (0, x_psuedo)),
+                        x1,
+                        x_psuedo,
+                        y1,
+                        -y2,
+                    )
                     add_c_shape(path, x_psuedo, x2, y2)
-
 
 
 def build_stupid_graph(gcode):
@@ -393,7 +497,7 @@ def build_stupid_graph(gcode):
     # The total number of crossings (n) is going to be 1/2 the total length of
     # the gauss code. We use integer divide to implicitly cast to an int so that
     # we can put it into `range(n)` later.
-    n = len(gcode)//2
+    n = len(gcode) // 2
 
     # Initialize the drawing path, which is currently a list of tuples
     # representing coordinates along the path that we are going to draw.
@@ -408,14 +512,14 @@ def build_stupid_graph(gcode):
     # This occurs when we have performed some "backtracking" in the knot, i.e.
     # when we encounter a crossing we've seen already before we've encountered
     # all crossings in the diagram. E.g., in the knot (6,4).
-    cross_x = [i*(n+3) for i in range(n)]
+    cross_x = [i * (n + 3) for i in range(n)]
 
     # Keep track of crossings we've seen already.
     cross_seen = [False for i in range(n)]
 
     for i in range(len(gcode) - 1):
         # Get information from the gauss code
-        c1, c2 = gcode[i], gcode[i+1]
+        c1, c2 = gcode[i], gcode[i + 1]
         c1i = get_cnum(c1) - 1
         c2i = get_cnum(c2) - 1
         x1, x2 = cross_x[c1i], cross_x[c2i]
@@ -436,17 +540,18 @@ def build_stupid_graph(gcode):
     get_path(path, cross_x, 0, 0, x1, 0, y1, 0)
 
     signs = []
-    for cnum in range(1,n+1):
+    for cnum in range(1, n + 1):
         # This is gross
         for c in gcode:
             stop = False
             if get_cnum(c) == cnum:
-                signs.append((c/abs(c)))
+                signs.append((c / abs(c)))
                 stop = True
             if stop:
                 break
 
     return path, cross_x, signs
+
 
 def space_xy(draw_path, cross_x):
     """
@@ -454,7 +559,7 @@ def space_xy(draw_path, cross_x):
     """
     x_vals = set()
     y_vals = set()
-    for x,y in draw_path:
+    for x, y in draw_path:
         x_vals.add((x))
         y_vals.add((y))
 
@@ -464,16 +569,17 @@ def space_xy(draw_path, cross_x):
     x0 = x_vals.index(0)
     y0 = y_vals.index(0)
 
-    x_map = {x: i-x0 for i,x in enumerate(x_vals)}
-    y_map = {y: i-y0 for i,y in enumerate(y_vals)}
+    x_map = {x: i - x0 for i, x in enumerate(x_vals)}
+    y_map = {y: i - y0 for i, y in enumerate(y_vals)}
 
     new_path = []
-    for (x,y) in draw_path:
+    for (x, y) in draw_path:
         new_path += [(x_map[x], y_map[y])]
 
-    new_cross_x = [x_map[x] for x  in cross_x]
+    new_cross_x = [x_map[x] for x in cross_x]
 
     return new_path, new_cross_x
+
 
 def draw_presentation(draw_paths, x_vals, signs, fname="test_gauss", display=True):
     """
@@ -485,7 +591,7 @@ def draw_presentation(draw_paths, x_vals, signs, fname="test_gauss", display=Tru
     x_vals is
     """
     n = len(x_vals)
-    break_width = .2
+    break_width = 0.2
 
     # Get the LaTeX preamble
     preamble = "\\documentclass[border=10pt]{standalone}\n"
@@ -510,8 +616,13 @@ def draw_presentation(draw_paths, x_vals, signs, fname="test_gauss", display=Tru
         else:
             drawing += f"\\draw[] ({x-break_width}, 0) -- ({x+break_width}, 0);\n"
 
-        drawing += f"\\node[circle, fill=black, draw=black, inner sep=1pt] () at ({x}, 0) " + "{};\n"
-        drawing += f"\\node[above left] () at ({x}, 0) " + "{$" + str(int(cnum+1)) + "$};\n"
+        drawing += (
+            f"\\node[circle, fill=black, draw=black, inner sep=1pt] () at ({x}, 0) "
+            + "{};\n"
+        )
+        drawing += (
+            f"\\node[above left] () at ({x}, 0) " + "{$" + str(int(cnum + 1)) + "$};\n"
+        )
 
     out_str = preamble + drawing + "\\end{tikzpicture}\n\\end{document}"
 
@@ -527,22 +638,69 @@ def draw_presentation(draw_paths, x_vals, signs, fname="test_gauss", display=Tru
     os.chdir("..")
     return out_str
 
-if __name__ == '__main__':
-    # for gc in gknot.keys():
-    #     knot = gknot[gc]
-    #     path, cross_x, signs = build_stupid_graph(knot)
-    #     path, cross_x = space_xy(path, cross_x)
-    #     try:
-    #         c,i = gc
-    #         draw_presentation([path], cross_x, signs, fname=f"{c}_{i}")
-    #     except TypeError:
-    #         draw_presentation([path], cross_x, signs, fname="0")
+
+def aggressive_tightener(path, cross_x):
+
+    return
+
+
+def get_shift_dict(gcode):
+    """
+    Create a dict where <val> indicates how many unique crossings we'll
+    encounter before backtracking if we perform <key> cyclic permutations of the
+    input Gauss code
+    """
+    n = len(gcode) // 2
+    len_dict = {i: 0 for i in range(n)}
+
+    for i in len_dict:
+        i_gcode = gcode[i:] + gcode[:i]
+        i_seen_cs = set()
+        for c in i_gcode:
+            cnum = get_cnum(c)
+            if cnum in i_seen_cs:
+                break
+            else:
+                len_dict[i] += 1
+                i_seen_cs.add(cnum)
+
+    sorted_dict = sorted(len_dict.items(), key=lambda kv: kv[1])
+    print("sorted_dict is", sorted_dict)
+    return sorted_dict
+
+
+def construct_all():
+    for gc in gknot.keys():
+        knot = gknot[gc]
+        sorted_dict = get_shift_dict(knot)
+
+        # We want to get all cyclic shift permutations as well
+        for shift_num in range(len(knot) // 2):
+            print(knot)
+            path, cross_x, signs = build_stupid_graph(knot)
+            path, cross_x = space_xy(path, cross_x)
+            try:
+                c, i = gc
+                draw_presentation([path], cross_x, signs, fname=f"{c}_{i}")
+            except TypeError:
+                draw_presentation([path], cross_x, signs, fname="0")
+
+            knot = knot[1:] + [knot[0]]
+
+
+if __name__ == "__main__":
+    construct_all()
 
     # Bad apples currently:
     # knot = gknot[(7,2)]
     # knot = gknot[(7,3)]
     # knot = gknot[(8,10)]
-    # knot = gknot[(8,18)]
+    # knot = gknot[(8,17)]
+
+    # path, cross_x, signs = build_stupid_graph(knot)
+    # path, cross_x = space_xy(path, cross_x)
+    # draw_presentation(path, cross_x, signs, fname="test_gauss")
+
     # ...and probably more, but that's where we get wrecked rn.
 
     # pathological_test = [
